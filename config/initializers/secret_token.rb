b66fe93f54797bee0df6289ed233303022bd11d6
stoms <github.20.spamgourmet.com>
2013-07-06 21:32:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6864343245269bfc85da897b8c081ca5c641073c2686238243aefabf46ffb8a28102abeeae1fe1715d6d6cc5edb808cedc91e738fef84ee86d0449e6103d46dd'
