606854554ac901134d1a24a2ff8012bb1228a6d0
stoms <github.20.spamgourmet.com>
2013-07-21 17:26:33 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
 
-  get "static_pages/help"
+  root to: 'static_pages#home'
 
-  get "static_pages/contact"
+  match '/help', to: 'static_pages#help', via: 'get'
 
-  get "static_pages/about"
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
+  match '/about', to: 'static_pages#about', via: 'get'
+
+  match '/signup', to: 'users#new', via: 'get'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
