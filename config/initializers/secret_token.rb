124b8f7783cdfdbf99191990af3bed81127abddc
stoms <github.20.spamgourmet.com>
2013-07-29 21:38:16 +0100

    Convert secret_token.rb so that secret key is not store in repository

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,19 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '6864343245269bfc85da897b8c081ca5c641073c2686238243aefabf46ffb8a28102abeeae1fe1715d6d6cc5edb808cedc91e738fef84ee86d0449e6103d46dd'
+#
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    File.read(token_file).chomp
+  else
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_token = secure_token
+#SampleApp::Application.config.secret_token = '6864343245269bfc85da897b8c081ca5c641073c2686238243aefabf46ffb8a28102abeeae1fe1715d6d6cc5edb808cedc91e738fef84ee86d0449e6103d46dd'
